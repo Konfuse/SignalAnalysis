bfa6414352416fd85f9a41d1f47e85cdb3f5cd9d
Konfuse <897896450@qq.com>
2019-05-10 19:31:15 +0800

    加入驱动加载类

--- signalanalysis/src/main/java/com/hust/ZTest.java
+++ signalanalysis/src/main/java/com/hust/ZTest.java
@@ -1,13 +1,10 @@
 package com.hust;
 
 import com.alibaba.fastjson.JSONObject;
+import com.hust.Util.DMBaseUtil;
 
-import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,36 +12,47 @@ import java.util.List;
  * @Date: 19-4-9 下午9:00
  */
 public class ZTest {
-    public static void main(String[] args) throws ParseException {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd");
+    private static final String JDBC_DRIVER = "dm.jdbc.driver.DmDriver";
+    private static final String DB_URL = "jdbc:dm://ypt.dameng.com:30033";
+    private static final String USER = "U_13109539713";
+    private static final String PASS = "8drVKOqWNq";
 
-        String result1 = simpleDateFormat.format(new Date());
-        System.out.println(result1);
-
-        int[][] arrays = new int[2][2];
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                arrays[i][j] = 1;
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        List<JSONObject> list = new ArrayList<>();
+        JSONObject jsonObject;
+        String target_name;
+        double rcs, height;
+        String sql = "SELECT t1.TARGET_TYPE, t1.TARGET_RCS, t1.TARGET_HEIGHT "
+                + "FROM DUCT_TARGET_PARA t1 ";
+        try {
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Connecting to DM database...");
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                jsonObject = new JSONObject();
+                target_name = resultSet.getString(1);
+                rcs = resultSet.getDouble(2);
+                height = resultSet.getDouble(3);
+                jsonObject.put("name", target_name);
+                jsonObject.put("rcs", rcs);
+                jsonObject.put("height", height);
+                list.add(jsonObject);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                DMBaseUtil.closeAll(connection, statement);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
-        System.out.println(Arrays.deepToString(arrays));
-
-        List<List<Integer>> lists = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            List<Integer> list = new ArrayList<>();
-            for (int j = 0; j < 2; j++) {
-                list.add(1);
-            }
-            lists.add(list);
-        }
-        System.out.println(lists);
-
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("1", arrays);
-        jsonObject.put("2", lists);
-        System.out.println(jsonObject.toJSONString());
-
-        double[] position = new double[0];
-        System.out.println(Arrays.toString(position));
+        System.out.println(list.toString());
     }
 }
